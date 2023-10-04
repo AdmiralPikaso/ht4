c464959f481fd8cb52e14ad906b858ca0e2d51bd
Баев Дмитрий <itspodarok@mail.ru>
2023-10-04 17:49:42 +0300

    Initial commit

---
+++ hello.pas
@@ -0,0 +1,8 @@
+﻿begin
+  var a := readstring('Choose language');
+  case a of
+    'Russian': print('Привет, мир!');
+    'English': print('Hello World!');
+    'German': print('Hallo Welt!');
+  end;
+end.
